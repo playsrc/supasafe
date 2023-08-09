3139abd0e166b7e90d018d33b6accad53e2ff186
Mateus Abelli <mateusabelli@gmail.com>
2023-08-08 21:51:57 -0300

    Initial commit

---
+++ src/App.svelte
@@ -0,0 +1,8 @@
+<script lang="ts">
+  import Counter from './lib/Counter.svelte'
+</script>
+
+<main class="p-2">
+  <h1 class="text-2xl font-bold mb-2 text-red-500">Vite + Svelte</h1>
+  <Counter />
+</main>
---
+++ src/lib/Counter.svelte
@@ -0,0 +1,10 @@
+<script lang="ts">
+  let count: number = 0
+  const increment = () => {
+    count += 1
+  }
+</script>
+
+<button class="bg-blue-500 p-2 rounded-md text-white" on:click={increment}>
+  count is {count}
+</button>
