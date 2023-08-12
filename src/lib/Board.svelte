0329caa2e13bbdd0d8e593665e1c8cb13baeef5e
Mateus Abelli <mateusabelli@gmail.com>
2023-08-11 22:35:26 -0300

    Add list logic

--- src/lib/Board.svelte
+++ src/lib/Board.svelte
@@ -1,4 +1,16 @@
 <script lang="ts">
+  let listItems = [1];
+
+  function addMore() {
+    listItems.push(listItems.length + 1);
+    listItems = listItems;
+  }
+
+  function deleteOne() {
+    if (listItems.length === 1) return;
+    listItems.pop();
+    listItems = listItems;
+  }
 </script>
 
 <div class="container">
@@ -10,12 +22,15 @@
     <p>I'd need to:</p>
   </div>
 
-  <ul class="ml-5 mt-5 list-disc flex flex-col gap-2">
+  <ul class="ml-5 mt-5 list-decimal flex flex-col gap-2">
+    {#each listItems as _}
+      <li>
+        <input class="w-full" type="text" placeholder="type here..." />
+      </li>
+    {/each}
     <li>
-      <input class="w-full" type="text" placeholder="type here..." />
-    </li>
-    <li>
-      <button>Insert more</button>
+      <button on:click={addMore}> 🌱 Add more</button>
+      <button on:click={deleteOne}>🔥 Delete one</button>
     </li>
   </ul>
 </div>
