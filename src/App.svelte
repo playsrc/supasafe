408ca9fe53894d4c673e43927292ed090af15d81
Mateus Abelli <mateusabelli@gmail.com>
2023-08-12 20:04:56 -0300

    Add loading state variable

--- src/App.svelte
+++ src/App.svelte
@@ -6,15 +6,16 @@
   import { onMount } from "svelte";
   import Auth from "./lib/Auth.svelte";
   import Board from "./lib/Board.svelte";
-  import Footer from "./lib/Footer.svelte";
   import Header from "./lib/Header.svelte";
   import { supabase } from "./supabaseClient";
 
   let session: AuthSession;
+  let loading = true;
 
   onMount(() => {
     supabase.auth.getSession().then(({ data }) => {
       session = data.session;
+      loading = false;
     });
 
     supabase.auth.onAuthStateChange((_event, _session) => {
@@ -26,9 +27,8 @@
 <Router>
   <Route path="/">
     <div class="container" style="padding: 50px 0 100px 0">
-      <Header {session} />
-      <Board />
-      <Footer {session} />
+      <Header {session} {loading} />
+      <Board {session} />
     </div>
   </Route>
   <Route path="/auth">
