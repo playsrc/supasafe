48f789d289e3bc474816a93dbd4445d3962258db
Mateus Abelli <mateusabelli@gmail.com>
2023-08-12 20:05:56 -0300

    Update loading states

--- src/lib/Header.svelte
+++ src/lib/Header.svelte
@@ -3,16 +3,26 @@
   import { supabase } from "../supabaseClient";
 
   export let session: AuthSession;
+  export let loading: boolean;
 
   let email = "";
 
   $: email = session?.user?.email || "loading...";
+
+  async function handleSignOut() {
+    await supabase.auth.signOut();
+    location.reload();
+  }
 </script>
 
 <div class="container">
   <h1 class="text-4xl leading-relaxed font-bold">Supasafe 🛡</h1>
 
-  {#if !session}
+  {#if loading}
+    <div class="flex gap-2 items-center">
+      <p>⏳ Loading...</p>
+    </div>
+  {:else if !session}
     <div class="flex gap-2 items-center">
       <p>You're currently not signed in</p>
       <a href="/auth" class="button"> 🔑 Sign In </a>
@@ -20,9 +30,7 @@
   {:else}
     <div class="flex gap-2 items-center">
       <p>Signed in as <span class="font-bold">{email}</span></p>
-      <button type="button" on:click={() => supabase.auth.signOut()}>
-        ❌ Sign Out
-      </button>
+      <button type="button" on:click={handleSignOut}>❌ Sign Out </button>
     </div>
   {/if}
   <hr class="w-full h-1 my-8 border-dashed border-gray-600" />
