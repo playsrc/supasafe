01eea6721ba5cffb0d13dfcbc518bdc9939e02f9
Mateus Abelli <mateusabelli@gmail.com>
2023-08-11 22:35:51 -0300

    Functional login states

--- src/lib/Header.svelte
+++ src/lib/Header.svelte
@@ -1,57 +1,29 @@
 <script lang="ts">
-  import { onMount } from "svelte";
   import type { AuthSession } from "@supabase/supabase-js";
   import { supabase } from "../supabaseClient";
 
   export let session: AuthSession;
 
-  const email = "email@example.com";
+  let email = "";
 
-  // let loading = false;
-  // let username: string | null = null;
-  // let website: string | null = null;
-  // let avatarUrl: string | null = null;
-
-  // onMount(() => {
-  //   getProfile();
-  // });
-
-  // const getProfile = async () => {
-  //   try {
-  //     loading = true;
-  //     const { user } = session;
-
-  //     const { data, error, status } = await supabase
-  //       .from("profiles")
-  //       .select("username, website, avatar_url")
-  //       .eq("id", user.id)
-  //       .single();
-
-  //     if (error && status !== 406) throw error;
-
-  //     if (data) {
-  //       username = data.username;
-  //       website = data.website;
-  //       avatarUrl = data.avatar_url;
-  //     }
-  //   } catch (error) {
-  //     if (error instanceof Error) {
-  //       alert(error.message);
-  //     }
-  //   } finally {
-  //     loading = false;
-  //   }
-  // };
+  $: email = session?.user?.email || "loading...";
 </script>
 
 <div class="container">
   <h1 class="text-4xl leading-relaxed font-bold">Supasafe 🛡</h1>
 
-  <div class="flex gap-2 items-center">
-    <p>Signed in as <span class="font-bold">{email}</span></p>
-    <button type="button" on:click={() => supabase.auth.signOut()}>
-      Sign Out
-    </button>
-  </div>
-  <hr class="w-full h-1 my-4 border-dashed border-gray-600" />
+  {#if !session}
+    <div class="flex gap-2 items-center">
+      <p>You're currently not signed in</p>
+      <a href="/auth" class="button"> 🔑 Sign In </a>
+    </div>
+  {:else}
+    <div class="flex gap-2 items-center">
+      <p>Signed in as <span class="font-bold">{email}</span></p>
+      <button type="button" on:click={() => supabase.auth.signOut()}>
+        ❌ Sign Out
+      </button>
+    </div>
+  {/if}
+  <hr class="w-full h-1 my-8 border-dashed border-gray-600" />
 </div>
